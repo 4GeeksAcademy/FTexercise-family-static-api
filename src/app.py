db29fa5ef5611086b7786f562cad96bac282e4c0
SrTorres07 <srtorresfelipe07@outlook.com>
2025-04-24 21:00:19 +0000

    proyecto terminado

--- src/app.py
+++ src/app.py
@@ -33,9 +33,34 @@ def sitemap():
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+        new_member = request.get_json()
+        member = jackson_family.add_member(new_member)
+        return jsonify(member), 200
+
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+     one_member = jackson_family.get_member(id)
+     if one_member:
+        return jsonify(one_member),200
+     return jsonify({"msg": "no found member"}),404
+
+    
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+     result = jackson_family.delete_member(id)
+     if result : 
+        return jsonify({"done": True}),200
+     return  jsonify({"done": False}),404
+
+
+     
+        
 
 
 
